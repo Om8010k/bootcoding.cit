325bb7d59a1b97f8a7eecb7aca9cc70c89a3b27d
Om8010k <kasadiwarom@gmail.com>
2023-08-18 14:06:50 +0530

    java

---
+++ java/hourstominute.java
@@ -0,0 +1,11 @@
+public class hourstominute {
+    public static void main(String[] args) {
+        int hour=4;
+        int min=10;
+         int sec = hour * 60 *60;
+        int secmin= min+60;
+        System.out.println("4hours in sec is " + sec);
+
+        System.out.println("secmin in seconds");
+    }
+}
