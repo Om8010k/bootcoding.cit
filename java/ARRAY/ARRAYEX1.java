d1dc657971ffd19cf56f8eb094e9d6394706c73c
Om8010k <kasadiwarom@gmail.com>
2023-08-19 12:07:06 +0530

    java

---
+++ java/ARRAY/ARRAYEX1.java
@@ -0,0 +1,17 @@
+package ARRAY;
+
+public class ARRAYEX1{
+    public static void main(String[] args) {
+        //Declare + initialize  Array
+        int[] temp;
+        // Declare +initialize Array
+        int[] arr = {5,9,15,2,8};
+
+        // Length of Arrray
+        int n = arr.length;
+        // traverse Array
+        for(int i = 0 ;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
